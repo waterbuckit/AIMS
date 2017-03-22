ea09fe64b86c5339769a64e0a378976f36e89397
waterbuckit <adamroyal232@yahoo.com>
2017-03-22 09:05:05 +0000

    Fixed the comparator for sorting a list of Bar objects

--- AIMS/src/aims/ReportBarChart.java
+++ AIMS/src/aims/ReportBarChart.java
@@ -5,12 +5,110 @@
  */
 package aims;
 
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 /**
  *
  * @author waterbucket
  */
-public class ReportBarChart extends JPanel{
+public class ReportBarChart extends JPanel {
+
+//    private final ArrayList<Bar> bars;
+    private final int MAXHEIGHT = 400;
+    private final int MAXWIDTH = 400;
+    public ReportBarChart(HashMap<String, Integer> values) {
+//        bars = makeBars(values);
+        this.setSize(500, 500);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        drawAxis(g2d, new Rectangle(this.getX(), this.getY()));
+    }
+
+    private ArrayList<Bar> makeBars(HashMap<String, Integer> values) {
+        // find the tallest bar
+        // make a comparator to sort from greatest to smallest
+        Comparator greatestToSmallest = (Comparator<Bar>) (Bar a, Bar b) -> b.getValue() - a.getValue();
+        ArrayList<Bar> ret = new ArrayList<>();
+
+        values.entrySet().forEach((entry) -> {
+            ret.add(new Bar(entry.getKey(), entry.getValue()));
+        });
+        ret.sort(greatestToSmallest);
+        //normalise and scale
+        int highestValue = ret.get(0).getValue();
+        ret.forEach((b) -> {
+            b.setValue((b.getValue()/highestValue)*MAXHEIGHT);
+        });
+        return ret;
+    }
+
+    private void drawAxis(Graphics2D g2d, Rectangle rectangle) {
+        // x axis
+        
+        g2d.drawLine(MAXWIDTH - 380 , 380, MAXWIDTH, 380);
+        g2d.drawString("Categories", MAXWIDTH / 2, 400);
+        // y axis
+        g2d.drawLine(MAXWIDTH - 380, 380, 20,20 );
+        Font font = new Font(null, Font.PLAIN, 12); 
+        AffineTransform affineTransform = new AffineTransform();
+        affineTransform.rotate(Math.toRadians(270), 0, 0);
+        Font rotatedFont = font.deriveFont(affineTransform);
+        g2d.setFont(rotatedFont);
+        g2d.drawString("Quantity", 13, 190);
+        g2d.dispose();
+    }
+
     
+    class Bar {
+
+        private String category;
+        private Integer value;
+
+        public Bar(String category, Integer value) {
+
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+        ReportBarChart panel = new ReportBarChart(new HashMap<String, Integer>() {{
+            put("Half", 20);
+            put("Oranges", 5);
+            put("Bananas", 40);
+            put("Quarter", 10);
+        }});
+        JFrame frame = new JFrame();
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
